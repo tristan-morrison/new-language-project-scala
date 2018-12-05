89312662131c6ff05f4d4f681184598bdc348db0
Tristan Lopus <tristan.lopus@gmail.com>
2018-12-05 08:21:34 -0500

    Cond now working

--- interpreter/src/main/scala/example/Hello.scala
+++ interpreter/src/main/scala/example/Hello.scala
@@ -7,9 +7,37 @@ object InterpreterRunner extends App {
   var subExprs = ArrayBuffer("")
   var currentExpr = 1
 
-  def interpret(expr: String) : Int = {
+  def interpret(expr: String) : String = {
     var desugaredInput = desugarMinus(expr)
     return parse(desugaredInput)
+  }
+
+  def cond(expr: String) : String = {
+    // for convenience, get rid of the final ) in the input expr
+    val input = expr.substring(0, expr.length - 1)
+    val exprList = input.split('[')
+    // exprList.foreach(println)
+
+    for (currentExpr <- exprList) {
+      // check to see what the first character of our test expression is
+      // if it's a #, then we know we have an already-evaluated boolean primitive --- i.e., #t or #f
+      // if it's a (, then we know we have an expression of form (= a b) that we need to evaluate
+      if (currentExpr.charAt(1) == 't') {
+        print("currentExpr: ")
+        println(currentExpr)
+        print("last index of space: ")
+        println(currentExpr.indexOf(' ') + 1)
+        print("last index of ]: ")
+        println(currentExpr.lastIndexOf(']'))
+        print("substring: ")
+        println(currentExpr.substring(3, 4))
+        return currentExpr.substring(currentExpr.indexOf(' ') + 1, currentExpr.lastIndexOf(']'))
+      } else if (currentExpr.substring(0, 4) == "else") {
+        return currentExpr.substring(currentExpr.indexOf(' ') + 1, currentExpr.lastIndexOf(']'))
+      }
+    }
+
+    return "Returned"
   }
 
   def desugarMinus(expr: String) : String = {
@@ -31,9 +59,11 @@ object InterpreterRunner extends App {
 
 
 
-  println(interpret(testExpr))
+  // println(interpret(testExpr))
+  // println(cond("(cond [(= 2 3) (+ 2 3)] [#f (- 5 7)])"))
+  println(parse("(+ 5 (cond [#f (+ 2 3)] [#f 22] [else 7]))"))
 
-  def parse(expr: String) : Int = {
+  def parse(expr: String) : String = {
     for (char <- expr) {
       if (char.equals('(')) {
         subExprs += ""
@@ -51,7 +81,8 @@ object InterpreterRunner extends App {
       }
     }
 
-    return subExprs(0).toInt
+    // return subExprs(0).toInt
+    return subExprs(0)
   }
 
   def eval(expr: String) : String = {
@@ -83,6 +114,10 @@ object InterpreterRunner extends App {
         } else {
           return exprArr(3).toString
         }
+      }
+      case "cond" => {
+        println(expr)
+        return cond(expr)
       }
     }
   }
